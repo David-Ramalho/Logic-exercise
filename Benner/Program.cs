27760217e1c8af0758956d7b8a2cffc9cd556875
David-Ramalho <tavaresramalho@hotmail.com>
2023-05-30 13:40:36 -0300

    Initial commit

---
+++ Benner/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using Benner.entities;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("*** Welcome to the Benner's Exercise ***\n");
+
+		int n;
+		do
+		{
+			Console.WriteLine("Enter the number of elements:");
+		} while (!int.TryParse(Console.ReadLine(), out n) || n <= 0);
+
+		Network elements = new Network(n);
+
+		ClearConsoleAndWait("Creating all the elements...");
+		ClearConsole();
+
+		Console.WriteLine("\nType the numbers to connect:");
+		int num1 = ReadIntegerInput("Number 1: ");
+		int num2 = ReadIntegerInput("Number 2: ");
+
+		elements.Connect(num1, num2);
+
+		ClearConsoleAndWait("Connecting the elements...");
+
+		Console.WriteLine("\nType the elements to check if they're connected:");
+		int qnum1 = ReadIntegerInput("Number 1: ");
+		int qnum2 = ReadIntegerInput("Number 2: ");
+
+		bool isConnected = elements.Query(qnum1, qnum2);
+
+		ClearConsole();
+		Console.WriteLine("Query Result:");
+		Console.WriteLine($"Elements {qnum1} and {qnum2} are {(isConnected ? "connected" : "not connected")}");
+
+		Console.ReadLine();
+	}
+
+	//Methods bellow.
+	static int ReadIntegerInput(string message)
+	{
+		int input;
+		bool isValidInput = false;
+		do
+		{
+			Console.Write(message);
+			isValidInput = int.TryParse(Console.ReadLine(), out input);
+		} while (!isValidInput);
+
+		return input;
+	}
+
+	static void ClearConsoleAndWait(string message)
+	{
+		ClearConsole();
+		Console.WriteLine("*** Welcome to the Benner's Exercise ***");
+		Console.WriteLine(message);
+		Thread.Sleep(2000);
+	}
+
+	static void ClearConsole()
+	{
+		Console.Clear();
+	}
+}
---
+++ Benner/entities/Network.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Benner.entities
+{
+	public class Network
+	{
+		private int elements;
+		private List<Tuple<int, int>> connections; // Field to store the connections
+
+		public Network(int elements) // Constructor
+		{
+			if (elements <= 0)
+			{
+				throw new ArgumentException("Write a positive integer number!");
+			}
+			this.elements = elements;
+			this.connections = new List<Tuple<int, int>>(); // Initialize the connections list
+		}
+
+		public void Connect(int num1, int num2) // First Method
+		{
+			if (num1 < 1 || num1 > elements || num2 < 1 || num2 > elements)
+			{
+				throw new ArgumentException("Elements must be within the range of 1 to " + elements + ".");
+			}
+
+			// Check if the connection already exists
+			foreach (var connection in connections)
+			{
+				if ((connection.Item1 == num1 && connection.Item2 == num2) || (connection.Item1 == num2 && connection.Item2 == num1))
+				{
+					throw new ArgumentException("Connection already exists.");
+				}
+			}
+
+
+			// Adding the connection to the connections list
+			connections.Add(new Tuple<int, int>(num1, num2));
+		}
+
+		public bool Query(int num1, int num2) // Second Method
+		{
+			if (num1 < 1 || num1 > elements || num2 < 1 || num2 > elements)
+			{
+				throw new ArgumentException("Elements must be within the range of 1 to " + elements + ".");
+			}
+
+			//Checking for direct connections
+			foreach (var connection in connections)
+			{
+				if ((connection.Item1 == num1 && connection.Item2 == num2) || (connection.Item1 == num2 && connection.Item2 == num1))
+				{
+					return true;
+				}
+			}
+			//Checking for indirect connections
+			foreach (var connection in connections)
+			{
+				if (connection.Item1 == num1)
+				{
+					if (Query(connection.Item2, num2))
+					{
+						return true;
+					}
+				}
+				else if (connection.Item2 == num1)
+				{
+					if (Query(connection.Item1, num2))
+					{
+						return true;
+					}
+				}
+
+			}
+			return false;
+
+		}
+	}
+}
